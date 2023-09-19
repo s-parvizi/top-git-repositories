3d8fd88739c15d8685aff685223b4c93c6f58c08
Soheil Parvizi <soheil.parvizi@gmail.com>
2023-09-19 23:31:45 +0200

    Add initial project structure

---
+++ src/main/java/com/git/top/topgit/TopGitApplication.java
@@ -0,0 +1,13 @@
+package com.git.top.topgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TopGitApplication.class, args);
+    }
+
+}
